e0a71c6a1c11842f3621ea9b907eb1708f062fdb
patron98 <jim.schyvens@gmail.com>
2022-03-22 16:05:52 +0100

    final

--- src/Main/Opdracht_18/BmiUtility.java
+++ src/Main/Opdracht_18/BmiUtility.java
@@ -12,19 +12,6 @@ public class BmiUtility {
     public static void printBmi (float bmi){
         System.out.println(bmi);
 
-        /*switch (bmi1){
-            case (bmi1 < 20) : System.out.println("Ondergewicht");
-                break;
-            case (bmi1 >= 20 && bmi1 < 25): System.out.println("Ok");
-                break;
-            case (bmi1 >= 25 && bmi1 < 30): System.out.println("Overgewicht");
-                break;
-            case (bmi1 >= 30 && bmi1 < 40): System.out.println("Obesitas");
-                break;
-            case (bmi1 > 40): System.out.println("Ziekelijk overgewicht");
-                break;
-        }*/
-
         if (bmi < 20){
             System.out.println("Ondergewicht");
         }else if (bmi >= 20 && bmi < 25){
